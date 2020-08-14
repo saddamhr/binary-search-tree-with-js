69ca3beaa1ba034fab08f23843cc183ef36653e1
saddamhr <saddambubt65@gmail.com>
2020-08-14 15:47:51 +0600

    remove node done

--- binary-search-tree-with-js.js
+++ binary-search-tree-with-js.js
@@ -29,6 +29,56 @@ class BinarySearchTree {
             else this.insertNode(node.right, newNode);
         }
     }
+
+    remove(data) {
+        this.root = this.removeNode(this.root, data);
+    }
+
+    removeNode(node, key) {
+        if(node === null) return null;
+        else if(key < node.data) {
+            node.left = this.removeNode(node.left, key);
+            return node;
+        } else if(key > node.data) {
+            node.right = this.removeNode(node.right, key);
+            return node;
+        } else {
+            if(node.left === null && node.right === null) {
+                node = null;
+                return node;
+            } if(node.left === null) {
+                node = node.right;
+                r
+                return node;
+            } else if(node.right === null) {
+                node = node.left;
+                return node;
+            }
+
+            var aux = this.findMinNode(node.right);
+            node.data = aux.data;
+
+            node.right = this.removeNode(node.right, aux.data);
+        }
+    }
+
+    findMinNode(node) {
+        if(node.left === null) {
+            return node;
+        } else return this.findMinNode(node.left);
+    }
+
+    getRootNode() {
+        return this.root;
+    }
+
+    inorder(node) {
+        if(node !== null) {
+            this.inorder(node.left);
+            console.log(node.data);
+            this.inorder(node.right);
+        }
+    }
 }
 
 const BST = new BinarySearchTree();
